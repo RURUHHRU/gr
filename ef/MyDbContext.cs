f56e5bf5bcdb0e4b56dbee9b27650326642f95bf
RURUHHRU <106974939+RURUHHRU@users.noreply.github.com>
2023-09-17 22:12:36 +0800

    Add files via upload

---
+++ ef/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace WpfApp1.ef
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<登录信息> _登录 { get; set; }
+        public DbSet<管理列表> _管理列表 { get; set; }
+        public DbSet<床位> _床位 { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=;User ID=sa;Password=;Database=桌面应用1;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+
+}
---
+++ ef/登录信息.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ef
+{
+   public class 登录信息
+    {
+        [Key]
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Passde { set; get; }
+    }
+    public class 管理列表
+    {
+        [Key]
+        public int Id { set; get; }
+        public string ?_1 { set; get; }
+        public string? _2 { set; get; }
+        public string? _3 { set; get; }
+        public string? _4 { set; get; }
+        public string? _5 { set; get; }
+        public string? _6 { set; get; }
+        public string? _7 { set; get; }
+        public string? _8 { set; get; }
+        public string? _9 { set; get; }
+
+    }
+
+    public class 床位
+    {
+        [Key]
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string 科室 { set; get; }
+        public string 床位1 { set; get; }
+        public string 床位号 { set; get; }
+    }
+    public class 管理绑定表
+    {
+        [Key]
+     
+        public string _1 { set; get; }
+        public string _2 { set; get; }
+        public string _3 { set; get; }
+        public string _4 { set; get; }
+        public string _5 { set; get; }
+        public string _6 { set; get; }
+        public string _7 { set; get; }
+        public string _8 { set; get; }
+        public string _9 { set; get; }
+
+    }
+}
