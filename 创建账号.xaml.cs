a0e5bdb14730514665218685c12006790fa52343
RURUHHRU <106974939+RURUHHRU@users.noreply.github.com>
2023-09-17 22:14:40 +0800

    Add files via upload

---
+++ 创建账号.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.ef;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// 创建账号.xaml 的交互逻辑
+    /// </summary>
+    public partial class 创建账号 : Page
+    {
+       
+        public 创建账号()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "确定密码"|| textBox.Text == "密码:字母或数字组成" || textBox.Text == "账号:字母或数字组成")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "密码:字母或数字组成";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+        private void TextBox_LostFocus1(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "账号:字母或数字组成";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+        private void TextBox_LostFocus2(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "确定密码";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(name.Text, "^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("请输入正确账号", "提示", MessageBoxButton.OK, MessageBoxImage.Error);
+                name.Text = "账号:字母或数字组成";
+                name.Foreground = Brushes.Gray; return;
+            }
+            else if (!Regex.IsMatch(password.Text, "^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("请输入正确密码", "提示", MessageBoxButton.OK, MessageBoxImage.Error);
+                textBox.Text = "密码:字母或数字组成";
+                textBox.Foreground = Brushes.Gray; return;
+            }
+            else if (password.Text != textBox.Text)
+            {
+                MessageBox.Show("两次密码错误","提示",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                using (var context = new MyDbContext())
+                {
+                    if (context._登录.FirstOrDefault(p => p.Name == name.Text) != null) { MessageBox.Show("账号已存在"); return; }
+                    context._登录.Add(new 登录信息
+                    {
+                        Name=name.Text,
+                        Passde=password.Text,
+                    } );
+                    context.SaveChanges();
+
+                    MessageBox.Show("创建成功,返回登录", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainWindow _dl = new MainWindow();
+                    _dl.Show();
+                    _dl.name.Text = name.Text;
+                    _dl.password.Text = password.Text;
+                    Window.GetWindow(this)?.Close();
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow _dl = new MainWindow();
+            _dl.Show();
+            Window.GetWindow(this)?.Close();
+        }
+    }
+
+    
+}
---
+++ 床位.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.ef;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// 床位.xaml 的交互逻辑
+    /// </summary>
+    public partial class 床位 : Window
+    {
+        public 床位()
+        {
+            InitializeComponent();
+            var T = new MyDbContext();
+            列表.ItemsSource = T._床位.OrderByDescending(x => x.Id).ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_1.Text != string.Empty && _3.Text != string.Empty && _7.Text != string.Empty)
+            {
+                using (var T = new MyDbContext())
+                {
+                    T._床位.Add(new ef.床位
+                    {
+                        Name = _1.Text,
+                        床位1 = _7.Text,
+                        床位号 = _3.Text,
+                        科室 = _2.Text,
+                    });
+                    T.SaveChanges();
+                    列表.ItemsSource= T._床位.OrderByDescending(x => x.Id).ToList();
+                }
+            }
+            else MessageBox.Show("请输入姓名，床位，床位号");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyDbContext context = new MyDbContext();
+            var query = context._床位.AsQueryable();
+
+            if (!string.IsNullOrEmpty(_1.Text))
+            {
+                query = query.Where(x => x.Name== _1.Text);
+            }
+            if (!string.IsNullOrEmpty(_7.Text))
+            {
+               
+                query = query.Where(x => x.床位1 == _7.Text);
+            }
+            if (!string.IsNullOrEmpty(_3.Text))
+            {
+                query = query.Where(x => x.床位号 == _3.Text);
+            }
+            if (!string.IsNullOrEmpty(_2.Text))
+            {
+              
+                query = query.Where(x => x.科室 == _2.Text);
+            }
+            列表.ItemsSource = query.ToList();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+                if (sender is Button editButton)
+                {
+                    if (editButton.DataContext is WpfApp1.ef.床位 dataRow)
+                    {
+                        using (var dbContext = new MyDbContext())
+                        {
+                            var entityToDelete = dbContext._床位.Find(dataRow.Id);
+
+                            if (entityToDelete != null)
+                            {
+                                if (MessageBox.Show("\t是否删除", "提示", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk) == MessageBoxResult.OK)
+                                {
+                                    dbContext._床位.Remove(entityToDelete);
+                                    dbContext.SaveChanges();
+                                    列表.ItemsSource = dbContext._床位.OrderByDescending(x => x.Id).ToList();
+                                }
+
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
